11cda24c0fabcafc10086229864f4fef12dd9bce
sucotechlive <sucotechlive@gmail.com>
2018-02-10 14:23:25 +0530

    first commit , added all the files

---
+++ styles.php
@@ -0,0 +1,4 @@
+ <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+ <link  href="node_modules/font-awesome/css/font-awesome.min.css" rel ="stylesheet" type="text/css">
+
+ <!-- Add your external css -->
