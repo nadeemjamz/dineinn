cb61f33f00c8006d17a95f81086a80f8504008b5
maktumhusen <altafpinjar22@gmail.com>
2018-02-13 10:48:41 +0530

    added 1 page

---
+++ hoteldesc.php
@@ -0,0 +1,72 @@
+  <!doctype html>
+<html lang="en">
+  <head>
+    <title>page title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!--styles css-->  
+    <?php include_once "styles.php"; ?>
+    <!--styles css--> 
+  </head>
+  <body style="background-color:#F4F4F4;">
+  <!--navbar-->
+  <?php include_once "header.php"; ?>
+
+
+  <!-- Start coding from here -->
+<div class="container">
+  <div class="row">
+
+    <div class="col-lg-9">
+      <br>
+      <div class="row"> 
+        <div class="col-lg-4">
+          <div id="carouselExampleControls0" class="carousel slide" data-ride="carousel" style="width:280px; height: 269px;">
+  <div class="carousel-inner"> 
+    <div class="carousel-item active">
+      <img class="d-block" src="images/the4.jpg" alt="First slide" style="width:100%;">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" src="images/the10.jpg" style="width:100%;" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block" src="images/img2.jpg" alt="Third slide" style="width:100%;">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls0" role="button" data-slide="prev" style="padding-bottom: 50px;">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls0" role="button" data-slide="next" style="padding-bottom: 50px;">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<div class="col-lg-8">
+<h5>Khan Saheb Grills and Rolls</h5>
+<p>Malleshwaram, West Bangalore</p>
+<p>₹ 500 for two (approx.)</p>
+<p>North Indian</p>
+<p>Tue -
+12:30 PM to 12:30 AM (Closed Now)View all</p> 
+
+</div>
+    </div>
+  </div>
+    <div class="col-lg-3">
+    </div>
+  </div>
+</div>
+
+
+
+
+  <!-- End coding from here -->
+  <!--footer-->
+  <?php include_once "footer.php"; ?>
+  <!-- end of footer-->    
+  <?php include_once "script.php"; ?>
+  </body>
+</html>
