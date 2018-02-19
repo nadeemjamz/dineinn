680474dde1779a6b3a73b027e9a01f839ba58f68
shweta <shwetasourceus2gmail.com>
2018-02-19 17:36:27 +0530

    added sign up files

--- header.php
+++ header.php
@@ -1,6 +1,3 @@
-
- 
-
  <nav class="navbar navbar-expand-lg navbar-light bg-danger sticky-top">
   <a class="navbar-brand" href="index.php" style="color:#fff;"><img src="images/dineinn8.png" class="img-fluid p-o m-0"> </a>
 <!--   <h4 class="mr-5">DineInn</h4> -->
@@ -69,7 +66,71 @@
                     <h5>Or login via </h5>
                     <hr>
                     <button class="btn btn-primary btn-inline-block mx-3">  <i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</button>
-                    <button class="btn btn-danger btn-inline-block mx-3"> <i class="fa fa-google" aria-hidden="true"></i> Sign in</button>
+                    <!-- <button class="btn btn-danger btn-inline-block mx-3"> <i class="fa fa-google" aria-hidden="true"></i> Sign in</button> -->
+
+                    <!-- php for signup -->
+                    <?php
+//Include GP config file && User class
+include_once 'gpConfig.php';
+include_once 'user.php';
+
+if(isset($_GET['code'])){
+    $gClient->authenticate($_GET['code']);
+    $_SESSION['token'] = $gClient->getAccessToken();
+    header('Location: ' . filter_var($redirectURL, FILTER_SANITIZE_URL));
+}
+
+if (isset($_SESSION['token'])) {
+    $gClient->setAccessToken($_SESSION['token']);
+}
+
+if ($gClient->getAccessToken()) {
+    //Get user profile data from google
+    $gpUserProfile = $google_oauthV2->userinfo->get();
+    
+    //Initialize User class
+    $user = new User();
+    
+    //Insert or update user data to the database
+    $gpUserData = array(
+        'oauth_provider'=> 'google',
+        'oauth_uid'     => $gpUserProfile['id'],
+        'first_name'    => $gpUserProfile['given_name'],
+        'last_name'     => $gpUserProfile['family_name'],
+        'email'         => $gpUserProfile['email'],
+        // 'gender'        => $gpUserProfile['gender'],
+        'locale'        => $gpUserProfile['locale'],
+        'picture'       => $gpUserProfile['picture'],
+        // 'link'          => $gpUserProfile['link']
+    );
+    $userData = $user->checkUser($gpUserData);
+    
+    //Storing user data into session
+    $_SESSION['userData'] = $userData;
+    
+    //Render facebook profile data
+    if(!empty($userData)){
+        $output = '<h1>Google+ Profile Details </h1>';
+        $output .= '<img src="'.$userData['picture'].'" width="300" height="220">';
+        $output .= '<br/>Google ID : ' . $userData['oauth_uid'];
+        $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
+        $output .= '<br/>Email : ' . $userData['email'];
+        // $output .= '<br/>Gender : ' . $userData['gender'];
+        $output .= '<br/>Locale : ' . $userData['locale'];
+        $output .= '<br/>Logged in with : Google';
+        // $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Google+ Page</a>';
+        $output .= '<br/>Logout from <a href="logout.php">Google</a>'; 
+    }else{
+        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
+    }
+} else {
+    $authUrl = $gClient->createAuthUrl();
+    $output = '<a href="'.filter_var($authUrl, FILTER_SANITIZE_URL).'"><img src="images/glogin.png" alt="" style="width:250px;"/></a>';
+}
+?>
+
+<div><?php echo $output; ?></div>
+                    <!-- end of code signup -->
                     </div>
                     </div>
                     </div>
---
+++ script1.php
@@ -0,0 +1,29 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="http://yourjavascript.com/21051171187/jquery-touchswipe-min.js"></script>
+
+<!-- MaterialDesign -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- custom script -->
+
+
+<script src="js/main.js"></script>
+
+<script src="js/ajax.js"></script>
+
+<script src="js/script.js"></script>
+
+<!-- <script src="js/sumaiya.js"></script> -->
+
+<script src="js/girish.js"></script>
+
+
